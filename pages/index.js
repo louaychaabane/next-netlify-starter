de596d14f5b76a9de9318bcbabbb9ec4881d9c94
louaychaabane <116122611+louaychaabane@users.noreply.github.com>
2022-10-18 22:43:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Louay Chaabane</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
